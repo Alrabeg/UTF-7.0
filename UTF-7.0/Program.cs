f6acb5eb8eb8982e877ed0d21b0bb23f5d59c011
Alrabeg <roman.stolypin1990@gmail.com>
2020-09-18 17:53:22 +0300

    Добавьте файлы проекта.

---
+++ UTF-7.0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Text;
+namespace UTF_7._0
+{
+  class Program
+    {
+      static void Main(string[] args)
+      {
+        StreamReader sr = new StreamReader(@"boot.ini");
+        StreamWriter sw = new StreamWriter(@"boot - utf7.txt", false, Encoding.UTF7);
+        sw.WriteLine(sr.ReadToEnd());
+        sw.Close();
+        sr.Close();
+
+      }
+    }
+  
+
+}
